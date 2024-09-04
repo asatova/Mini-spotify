d526a7e7023c3cada981672d14f3c7b389a89f1b
zarnigor <zarnigorasatova52@gmail.com>
2024-09-04 13:26:33 +0500

    feature

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import React, { useState } from "react";
-
-// Corrected imports
 import Song from "./componets/Song";
 import Player from "./componets/Player";
 import "./styles/app.scss";
-
-// Assuming data is a function that returns an array
 import data from "./util";
 
 function App() {
-  const [songs, setSongs] = useState(data()); // Ensure data() is used if data is a function
-  const [currentSong, setCurrentSong] = useState(songs[0]);
+    const [songs, setSongs] = useState(data());
+    const [currentSong, setCurrentSong] = useState(songs[0]);
+    const [isPlaying, setIsPlaying] = useState(false);
 
-  return (
-    <div className="App">
-      <Song currentSong={currentSong} />
-      <Player />
-    </div>
-  );
+    return (
+        <div className="App">
+            <Song currentSong={currentSong} />
+            <Player
+                currentSong={currentSong}
+                isPlaying={isPlaying}
+                setIsPlaying={setIsPlaying}
+            />
+        </div>
+    );
 }
 
 export default App;
-
--- src/componets/Player.js
+++ src/componets/Player.js
@@ -1,22 +1,56 @@
-import React from "react";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faAngleLeft, faAngleRight, faPlay} from "@fortawesome/free-solid-svg-icons";
+import React, {useRef, useState} from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAngleLeft, faAngleRight, faPlay } from "@fortawesome/free-solid-svg-icons";
 
-const Player = () => {
-    return(
-        <div className="player">
-           <div className="time-control">
-               <p>Start time</p>
-               <input type="range"  />
-               <p>End time</p>
-           </div>
-            <div className="player-control">
-                <FontAwesomeIcon className="skip-back" icon={faAngleLeft} size="2x"/>
-                <FontAwesomeIcon className="play" icon={faPlay} size="2x"/>
-                <FontAwesomeIcon className="skip-forward" icon={faAngleRight} size="2x" />
+const Player = ({ currentSong, isPlaying, setIsPlaying }) => {
+    const audioRef = useRef(null);
 
-            </div>
-        </div>
-    )
+    // Event handler for play/pause button
+    const playSongHandler = () => {
+        if (isPlaying) {
+            audioRef.current.pause();
+            setIsPlaying(false);
+        } else {
+            audioRef.current.play();
+            setIsPlaying(true);
+        }
+    };
+const timeUpdateHandler = (e) => {
+    const current = e.target.currentTime;
+    const duration = e.target.duration;
+    setSongInfo({
+        ...songInfo,
+        currentTime: current,
+        duration,
+    })
+};
+
+const getTime = (time) => {
+    return Math.floor(time / 60) + ":" + ("0" + Math.floor(time % 60)).slice(-2)
 }
+
+
+    //State
+    const [songInfo, setSongInfo] = useState({
+        currentTime:null,
+        duration:null,
+    });
+
+    return (
+        <div className="player">
+            <div className="time-control">
+                <p>{getTime(songInfo.currentTime)}</p>
+                <input type="range" />
+                <p>{getTime(songInfo.duration)}</p>
+            </div>
+            <div className="player-control">
+                <FontAwesomeIcon className="skip-back" icon={faAngleLeft} size="2x" />
+                <FontAwesomeIcon onClick={playSongHandler} className="play" icon={faPlay} size="2x" />
+                <FontAwesomeIcon className="skip-forward" icon={faAngleRight} size="2x" />
+            </div>
+            <audio onLoadedData={timeUpdateHandler} onTimeUpdate={timeUpdateHandler} ref={audioRef} src={currentSong.audio}></audio>
+        </div>
+    );
+};
+
 export default Player;
